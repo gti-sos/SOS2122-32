1863f8db618837ecc626e9f996a905b5e8cb7bb8
rafamolino <35599091+rafamolino@users.noreply.github.com>
2022-03-27 20:16:58 +0200

    cambios en la api

--- index.js
+++ index.js
@@ -8,37 +8,127 @@ app.use(bodyParser.json());
 const BASE_API_URL = "/api/v1";
 
 //######################   API Rafael Molino Alvarez  ###############################//
-var repeatersStats = [
-    {
-        country:"Albanian",
-        year : 2011,
-        women : 0.725,
-        men : 1.005,
-        average : 0.865
+var repeatersStats = [];
 
-    },
-
-    {
-        country:"Spain",
-        year : 2010,
-        women : 2.1,
-        men : 2.986,
-        average : 2.5
-    }
-];
-
-app.get(BASE_API_URL+"/repeatersStats", (req,res)=>{
-
+app.get(BASE_API_URL+"/repeaters-stats", (req,res)=>{
     res.send(JSON.stringify(repeatersStats,null,2));
 });
 
+app.get(BASE_API_URL+"/repeaters-stats/:name", (req,res)=>{
+    var repeatersStats1 = [
+        {
+            country:"Albanian",
+            year : 2011,
+            women : 0.725,
+            men : 1.005,
+            average : 0.865
+    
+        },
+    
+        {
+            country:"Chile",
+            year : 2015,
+            women : 2.665,
+            men : 4.501,
+            average : 3.583
+    
+        },
+    
+        {
+            country:"Belgium",
+            year : 2013,
+            women : 2.532,
+            men : 2.682,
+            average : 2.6
+    
+        },
+    
+        {
+            country:"Ireland",
+            year : 2005,
+            women : 0.299,
+            men : 0.36,
+            average : 0.330
+    
+        },
+    
+        {
+            country:"Argentina",
+            year : 2013,
+            women : 2.283,
+            men : 3.39,
+            average : 2.837
+        }
+    ];
 
+    repeatersName = repeatersStats.filter((h)=>{
+        return(h.country==req.params.name)
+    });
 
-app.post(BASE_API_URL+"/repeatersStats", (req,res)=>{
+    if(req.params.name == "loadInitialData" && repeatersStats==0){
+        repeatersStats = repeatersStats1;
+        res.sendStatus(200, "OK")
+    } else if(repeatersName == 0){
+        res.sendStatus(404, "NOT FOUND");
+    } else{
+        res.send(JSON.stringify(repeatersName,null,2));
+    }
+});
 
-    repeatersStats.push(req.body);
+app.post(BASE_API_URL+"/repeaters-stats", (req,res)=>{
+    if(req.body.country && parseInt(req.body.year) && parseFloat(req.body.men) && parseFloat(req.body.women) && parseFloat(req.body.average) && Object.keys(req.body).length==5){
+    result = repeatersStats.filter((h)=>{
+        return(JSON.stringify(h,null,2)===JSON.stringify(req.body,null,2));
+    });
+    if(result != 0){
+        res.sendStatus(409,"CONFLICT");
+    }else{
+        repeatersStats.push(req.body);
+        res.sendStatus(201,"CREATED");
+    }
+}else{
+    res.sendStatus(400,"BAD REQUEST");
+}
+});
 
+app.post(BASE_API_URL+"/repeaters-stats/:name", (req,res)=>{
+    res.sendStatus(405,"METHOD NOT ALLOWED");
+});
+
+app.put(BASE_API_URL+"/repeaters-stats", (req,res)=>{
+    res.sendStatus(405,"METHOD NOT ALLOWED");
+});
+
+app.put(BASE_API_URL+"/repeaters-stats/:name", (req,res)=>{
+    if(req.body.country && parseInt(req.body.year) && parseFloat(req.body.men) && parseFloat(req.body.women) && parseFloat(req.body.average) && Object.keys(req.body).length==5){
+    if(req.params.name==req.body.country){
+        repeatersStats = repeatersStats.map((h)=>{
+        if(h.country==req.params.name){
+            return(req.body);
+        }else{
+            return h;
+        }
+    })
     res.sendStatus(201,"CREATED");
+}
+    else{
+    res.sendStatus(400,"BAD REQUEST");
+    }
+}else{
+    res.sendStatus(400,"BAD REQUEST");
+}
+});
+
+app.delete(BASE_API_URL+"/repeaters-stats", (req,res)=>{
+    houseworkStats = []
+    res.sendStatus(200,"OK");
+});
+
+app.delete(BASE_API_URL+"/repeaters-stats/:name", (req,res)=>{
+    repeatersStats = repeatersStats.filter((h)=>{
+        return(h.country!=req.params.name)
+    })
+    res.sendStatus(200,"OK");
 });
 
 //######################   API Javier Hidalgo García  ###############################//
@@ -164,6 +254,9 @@ app.delete(BASE_API_URL+"/housework-stats/:name", (req,res)=>{
     })
     res.sendStatus(200,"OK");
 });
+
+//######################   API ****  ###############################//
+
 
 
 app.use("/", express.static('public'));
