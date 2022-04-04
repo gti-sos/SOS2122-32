3e75a7d9893e07c83516f2777b2d688d89947908
rafamolino <35599091+rafamolino@users.noreply.github.com>
2022-04-04 14:24:36 +0200

    correccion tests

--- src/back/rafmolalvAPI/indexRma.js
+++ src/back/rafmolalvAPI/indexRma.js
@@ -1,93 +1,140 @@
 const bodyParser = require("body-parser");
 const BASE_API_URL = "/api/v1";
+const RMA_BASE_API_URL = "/api/v1/repeaters-stats";
+
 
 var repeatersStats = [];
+
+var initialStats = [
+    {
+        country:"Albanian",
+        year : 2011,
+        women : 0.725,
+        men : 1.005,
+        average : 0.865
+
+    },
+
+    {
+        country:"Chile",
+        year : 2015,
+        women : 2.665,
+        men : 4.501,
+        average : 3.583
+
+    },
+
+    {
+        country:"Belgium",
+        year : 2013,
+        women : 2.532,
+        men : 2.682,
+        average : 2.6
+
+    },
+    {
+        country:"Belgium",
+        year : 2016,
+        women : 2.532,
+        men : 2.682,
+        average : 2.6
+
+    },
+
+    {
+        country:"Ireland",
+        year : 2005,
+        women : 0.299,
+        men : 0.36,
+        average : 0.330
+
+    },
+
+    {
+        country:"Argentina",
+        year : 2013,
+        women : 2.283,
+        men : 3.39,
+        average : 2.837
+    }
+];
+
 module.exports = (app) =>{
+    app.use(bodyParser.json());
 
- 
 
-app.get(BASE_API_URL+"/repeaters-stats", (req,res)=>{
-    res.send(JSON.stringify(repeatersStats,null,2));
-});
-
-app.get(BASE_API_URL+"/repeaters-stats/:name", (req,res)=>{
-    var repeatersStats1 = [
-        {
-            country:"Albanian",
-            year : 2011,
-            women : 0.725,
-            men : 1.005,
-            average : 0.865
-    
-        },
-    
-        {
-            country:"Chile",
-            year : 2015,
-            women : 2.665,
-            men : 4.501,
-            average : 3.583
-    
-        },
-    
-        {
-            country:"Belgium",
-            year : 2013,
-            women : 2.532,
-            men : 2.682,
-            average : 2.6
-    
-        },
-    
-        {
-            country:"Ireland",
-            year : 2005,
-            women : 0.299,
-            men : 0.36,
-            average : 0.330
-    
-        },
-    
-        {
-            country:"Argentina",
-            year : 2013,
-            women : 2.283,
-            men : 3.39,
-            average : 2.837
-        }
-    ];
-
-    repeatersName = repeatersStats.filter((h)=>{
-        return(h.country==req.params.name)
+    //Documentacion
+    app.get(RMA_BASE_API_URL+"/docs",(req,res)=>{
+        res.redirect("https://documenter.getpostman.com/view/20237623/UVyoVHPy");
     });
 
-    if(req.params.name == "loadInitialData" && repeatersStats==0){
-        repeatersStats = repeatersStats1;
-        res.sendStatus(200, "OK")
-    } else if(repeatersName == 0 && req.params.name != "docs"){
-        res.sendStatus(404, "NOT FOUND");
-    } else if(req.params.name == "docs"){
-        res.redirect("https://documenter.getpostman.com/view/20237623/UVyoVHPy");
-    } 
-    else{
-        res.send(JSON.stringify(repeatersName,null,2));
-    }
-});
-//Busqueda por pais y año
-app.get(BASE_API_URL+"/repeaters-stats/:country/:year", (req, res)=>{
-    var countryName = req.params.country;
-    var year = req.params.year;
-    filteredCountriesYears = repeatersStats.filter((r)=>{
-        return(r.country == countryName && r.year == year);
-    })
-    if(filteredCountriesYears == 0){
-        res.sendStatus(404,"NOT FOUND");
-    }else{
-        res.send(JSON.stringify(filteredCountriesYears[0],null,2));
-    }
+    //LoadInitialData
 
-    res.sendStatus(200,"OK");
-});
+    app.get(RMA_BASE_API_URL + "/loadInitialData", (req,res) => {
+
+
+        if(repeatersStats.length === 0){
+            initialStats.forEach((a)=>{
+                repeatersStats.push(a);
+            });
+        }
+        res.send(JSON.stringify(repeatersStats,null,2));
+        
+    });
+
+    //Busqueda por pais
+
+    app.get(RMA_BASE_API_URL+"/:country", (req, res) => {
+        var Country = req.params.country;
+        filtered = repeatersStats.filter((e) => {
+            return (e.country == Country);
+        });
+        if (filtered == 0) {
+            res.sendStatus(404, "NOT FOUND");
+        }
+        else {
+            res.send(JSON.stringify(filtered, null, 2));
+        }
+    })
+
+
+    //Busqueda por pais y año
+    app.get(RMA_BASE_API_URL+"/:country/:year", (req,res) => {
+        var Country = req.params.country;
+        var Year = req.params.year;
+        filtered = repeatersStats.filter( (e) => {
+            return ((e.country == Country) && (e.year == Year));
+        });
+        if(filtered == 0){
+            res.sendStatus(404,"NOT FOUND");
+        }
+        else{
+            res.send(JSON.stringify(filtered[0],null,2));
+        }
+    });
+
+    //Busqueda general
+    
+    app.get(BASE_API_URL+"/repeaters-stats", (req,res) => {
+
+        if(repeatersStats == []){
+            res.sendStatus(404,"NOT FOUND");
+        }else{
+            res.send(JSON.stringify(repeatersStats,null,2));
+        }
+        
+        res.sendStatus(200,"OK");
+    
+        //paginacion
+        if (req.query.limit != undefined || req.query.offset != undefined) {
+            filteredList = paginacion(req, filteredList);
+        }
+        filteredList.forEach((element) => {
+            delete element._id;
+        });
+        res.send(JSON.stringify(filteredList, null, 2));
+    });
+
 
 
 
