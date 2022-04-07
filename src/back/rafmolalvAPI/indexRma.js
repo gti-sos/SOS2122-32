dd37e5da276f299b9b1983584e7a929af9ce82d3
rafamolino <35599091+rafamolino@users.noreply.github.com>
2022-04-07 02:26:37 +0200

    edit

--- src/back/rafmolalvAPI/indexRma.js
+++ src/back/rafmolalvAPI/indexRma.js
@@ -137,140 +137,140 @@ module.exports = (app, db) => {
                 else {
                     // existen ambos
                     if (from != null && to != null) {
-                            db.find({ $and: [{ year: { $gte: parseInt(from) } }, { year: { $lte: parseInt(to) } }] }, function (err, docs) {
-                                if (err) {
-                                    res.sendStatus(500, "INTERNAL SERVER ERROR");
-                                }
-                                else {
-                                    docsCopy = docs;
-                                    docsCopy.forEach((e) => {
-                                        delete e._id;
-                                    });
-                                    res.send(JSON.stringify(docsCopy, null, 2));
-                                }
-                            });
-                        
+                        db.find({ $and: [{ year: { $gte: parseInt(from) } }, { year: { $lte: parseInt(to) } }] }, function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
+                            }
+                            else {
+                                docsCopy = docs;
+                                docsCopy.forEach((e) => {
+                                    delete e._id;
+                                });
+                                res.send(JSON.stringify(docsCopy, null, 2));
+                            }
+                        });
+
                     }
                     //existe to pero no from
                     else if (from == null && to != null) {
-                        
-                            db.find({ $or: [{ year: { $lte: parseInt(to) } }] }, function (err, docs) {
-                                if (err) {
-                                    res.sendStatus(500, "INTERNAL SERVER ERROR");
-                                }
-                                else {
-                                    docsCopy = docs;
-                                    docsCopy.forEach((e) => {
-                                        delete e._id;
-                                    });
-                                    res.send(JSON.stringify(docsCopy, null, 2));
-                                }
-                            });
-                        
-                       
+
+                        db.find({ $or: [{ year: { $lte: parseInt(to) } }] }, function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
+                            }
+                            else {
+                                docsCopy = docs;
+                                docsCopy.forEach((e) => {
+                                    delete e._id;
+                                });
+                                res.send(JSON.stringify(docsCopy, null, 2));
+                            }
+                        });
+
+
                     }
                     //EXISTE EL FROM PERO NO EL TO
                     else {
                         // NO HAY OFFSET Y LIMIT
-                            db.find({ $or: [{ year: { $gte: parseInt(from) } }] }, function (err, docs) {
-                                if (err) {
-                                    res.sendStatus(500, "INTERNAL SERVER ERROR");
-                                }
-                                else {
-                                    docsCopy = docs;
-                                    docsCopy.forEach((e) => {
-                                        delete e._id;
-                                    });
-                                    res.send(JSON.stringify(docsCopy, null, 2));
-                                }
-                            });
-                        
-                       
-                        
+                        db.find({ $or: [{ year: { $gte: parseInt(from) } }] }, function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
+                            }
+                            else {
+                                docsCopy = docs;
+                                docsCopy.forEach((e) => {
+                                    delete e._id;
+                                });
+                                res.send(JSON.stringify(docsCopy, null, 2));
+                            }
+                        });
+
+
+
                     }
                 }
             }
             //no hay ni from ni to
-            else{ 
-    
-                if(year == null){
+            else {
+
+                if (year == null) {
                     //ni offset ni limit
-                    if(offset == undefined && limit == undefined){
-                        db.find({}, function(err, docs){
-                            if(err){
-                                res.sendStatus(500,"INTERNAL SERVER ERROR");
+                    if (offset == undefined && limit == undefined) {
+                        db.find({}, function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
                             }
-                            else{
+                            else {
                                 docsCopy = docs;
                                 docsCopy.forEach((e) => {
-                                delete e._id;
+                                    delete e._id;
                                 });
                                 res.send(JSON.stringify(docsCopy, null, 2));
                             }
                         });
                     }
                     //offset y no limit
-                    else if(offset != undefined && limit == undefined){
-                        db.find({}).skip(offset).exec(function(err, docs){
-                            if(err){
-                                res.sendStatus(500,"INTERNAL SERVER ERROR");
+                    else if (offset != undefined && limit == undefined) {
+                        db.find({}).skip(offset).exec(function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
                             }
-                            else{
+                            else {
                                 docsCopy = docs;
                                 docsCopy.forEach((e) => {
-                                delete e._id;
+                                    delete e._id;
                                 });
                                 res.send(JSON.stringify(docsCopy, null, 2));
                             }
                         });
                     }
                     //limit y no offset
-                    else if(offset == undefined && limit != undefined){
-                        db.find({}).limit(limit).exec(function(err, docs){
-                            if(err){
-                                res.sendStatus(500,"INTERNAL SERVER ERROR");
+                    else if (offset == undefined && limit != undefined) {
+                        db.find({}).limit(limit).exec(function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
                             }
-                            else{
+                            else {
                                 docsCopy = docs;
                                 docsCopy.forEach((e) => {
-                                delete e._id;
+                                    delete e._id;
                                 });
                                 res.send(JSON.stringify(docsCopy, null, 2));
                             }
                         });
                     }
                     //offset y limit
-                    else{
-                        db.find({}).limit(limit).skip(offset).exec(function(err, docs){
-                            if(err){
-                                res.sendStatus(500,"INTERNAL SERVER ERROR");
+                    else {
+                        db.find({}).limit(limit).skip(offset).exec(function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
                             }
-                            else{
+                            else {
                                 docsCopy = docs;
                                 docsCopy.forEach((e) => {
-                                delete e._id;
+                                    delete e._id;
                                 });
                                 res.send(JSON.stringify(docsCopy, null, 2));
                             }
                         });
                     }
                 }
-                else{ 
-                    if(offset == undefined && limit == undefined){
-                        db.find({year: parseInt(year)}, function(err, docs){
-                            if(err){
-                                res.sendStatus(500,"INTERNAL SERVER ERROR");
+                else {
+                    if (offset == undefined && limit == undefined) {
+                        db.find({ year: parseInt(year) }, function (err, docs) {
+                            if (err) {
+                                res.sendStatus(500, "INTERNAL SERVER ERROR");
                             }
-                            else{
+                            else {
                                 docsCopy = docs;
                                 docsCopy.forEach((e) => {
-                                delete e._id;
+                                    delete e._id;
                                 });
                                 res.send(JSON.stringify(docsCopy, null, 2));
                             }
                         });
                     }
-                    
+
                 }
             }
         }
@@ -281,25 +281,25 @@ module.exports = (app, db) => {
 
     //Busqueda por pais
 
-    app.get(RMA_BASE_API_URL+"/:country", (req, res) => {
+    app.get(RMA_BASE_API_URL + "/:country", (req, res) => {
         var Country = req.params.country;
-         db.find({country: Country},function(err, docs){
+        db.find({ country: Country }, function (err, docs) {
 
-            if(err){
+            if (err) {
                 res.sendStatus(500, "ERROR IN CLIENT");
                 return;
             }
 
-            else{
-                if(docs == 0){
+            else {
+                if (docs == 0) {
                     res.sendStatus(404, "NOT FOUND")
                 }
-                else{
+                else {
                     docsCopy = docs;
                     docsCopy.forEach((e) => {
                         delete e._id;
                     })
-                    res.send(JSON.stringify(docsCopy,null,2));
+                    res.send(JSON.stringify(docsCopy, null, 2));
                 }
             }
 
@@ -311,23 +311,23 @@ module.exports = (app, db) => {
     app.get(RMA_BASE_API_URL + "/:country/:year", (req, res) => {
         var Country = req.params.country;
         var Year = req.params.year;
-         db.find({country: Country, year:parseInt(Year)},function(err, docs){
+        db.find({ country: Country, year: parseInt(Year) }, function (err, docs) {
 
-            if(err){
+            if (err) {
                 res.sendStatus(500, "ERROR IN CLIENT");
                 return;
             }
 
-            else{
-                if(docs == 0){
+            else {
+                if (docs == 0) {
                     res.sendStatus(404, "NOT FOUND")
                 }
-                else{
+                else {
                     docsCopy = docs;
                     docsCopy.forEach((e) => {
                         delete e._id;
                     })
-                    res.send(JSON.stringify(docsCopy,null,2));
+                    res.send(JSON.stringify(docsCopy, null, 2));
                 }
             }
 
@@ -338,7 +338,7 @@ module.exports = (app, db) => {
 
 
 
-    function comprobacion_parametros(req){
+    function comprobacion_parametros(req) {
         return (req.body.country == null |
             req.body.year == null |
             req.body.women == null |
@@ -350,23 +350,23 @@ module.exports = (app, db) => {
     app.post(BASE_API_URL + "/repeaters-stats", (req, res) => {
 
 
-        if(comprobacion_parametros(req)){
-            res.sendStatus(400,"BAD REQUEST");
+        if (comprobacion_parametros(req)) {
+            res.sendStatus(400, "BAD REQUEST");
         }
-        else{
-            db.find({},function(err,registro){
-                if(err){
-                    res.sendStatus(500,"INTERNAL SERVER ERROR");
+        else {
+            db.find({}, function (err, registro) {
+                if (err) {
+                    res.sendStatus(500, "INTERNAL SERVER ERROR");
                 }
 
-                registro = registro.filter((reg)=>{
-                    return(req.body.country==reg.country && req.body.year == reg.year)
+                registro = registro.filter((reg) => {
+                    return (req.body.country == reg.country && req.body.year == reg.year)
                 })
-                if(registro.length != 0){
-                    res.sendStatus(409,"CONFLICT");
-                }else{
+                if (registro.length != 0) {
+                    res.sendStatus(409, "CONFLICT");
+                } else {
                     db.insert(req.body);
-                    res.sendStatus(201,"CREATED");
+                    res.sendStatus(201, "CREATED");
                 }
 
             })
@@ -388,40 +388,47 @@ module.exports = (app, db) => {
 
     app.put(BASE_API_URL + "/repeaters-stats/:country/:year", (req, res) => {
 
-        if(comprobacion_parametros(req)){
-            res.sendStatus(400,"BAD REQUEST");
+        if (comprobacion_parametros(req)) {
+            res.sendStatus(400, "BAD REQUEST");
         }
 
-        n_country = req.params.country;
-        n_year = req.params.year;
-        n_body = req.params.body;
+        else{
+            var n_country = req.params.country;
+            var n_year = req.params.year;
+            var n_body = req.body;
 
-        db.find({},function(err,registro){
-            if(err){
-                res.sendStatus(500,"INTERNAL SERVER ERROR");
-                return;
+            if(n_country != n_body.country || n_year != n_body.year){
+                res.sendStatus(400, "BAD REQUEST");
             }
+            else{
+                db.find({country: n_country, year: parseInt(n_year)}, function(err,docs){
+                    if(err){
+                        res.sendStatus(500,"INTERNAL SERVER ERROR");
+                    }
+                    else{
+                        if(docs==0){
+                            res.sendStatus(404, "NOT FOUND");
+                        }
+                        else{
+                            db.update({country: n_country, year: parseInt(n_year)},
+                            { $set: {
+                                "women": n_body.women,
+                                "men": n_body.men,
+                                "average": n_body.average
+                            }},function(err,newDocs){
+                                if(err){
+                                    res.sendStatus(500,"INTERNAL SERVER ERROR");
+                                }
+                                else{
+                                    res.sendStatus(200, "OK");
+                                }
+                            });
+                        }
+                    }
+                })
+            }
+        }
 
-            registro = registro.filter((reg)=>{
-                return(n_country==reg.country && n_year == reg.year)//se comprueba si existe ya ese dato
-            })
-            if(registro.length != 0){
-                res.sendStatus(404,"NOT FOUND");
-                return;
-            }
-            if(n_country != n_body.country || n_year != n_body.year){//se comrpueba si los nuevos datos son iguales a los que ya habia
-                res.sendStatus(400,"BAD REQUEST");
-                return;
-            }
-
-            db.update({$and:[{country: String(n_country)}, {year: parseInt(n_year)}]}, {$set: n_body}, {},function(err, nU) {
-                if (err) {
-                    res.sendStatus(500, "INTERNAL SERVER ERROR");
-                }else{
-                    res.sendStatus(200,"UPDATED");
-                }
-            });
-        })
     });
 
     //Borrar todos los datos
