d75b09d10f0276218ab50ded52fdc222709f7de0
rafamolino <35599091+rafamolino@users.noreply.github.com>
2022-04-17 23:15:58 +0200

    front

---
+++ front/ApiRMA.svelte
@@ -0,0 +1,52 @@
+<script>
+    import{onMount} from 'svelte';
+    let repeaters=[];
+    onMount(getrepeaters);
+    
+    async function getrepeaters(){
+        console.log("Fetching ending...");
+        const res = await fetch("/api/v1/repeaters-stats")
+        repeaters=await res.json();
+        console.log("Recevider: "+ repeaters.length);
+
+    }
+</script>
+<main>
+{#await repeaters}
+loading
+{:then repeaters}
+
+<table>
+	<thead>
+		<tr>
+			<th>
+				Country
+			</th>
+			<th>
+				Year
+			</th>
+			<th>
+				Women
+			</th>
+			<th>
+				Men
+			</th>
+			<th>
+				Average
+			</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each repeaters as rep}
+			<tr>
+				<td>{rep.country}</td>
+				<td>{rep.year}</td>
+				<td>{rep.women}</td>
+				<td>{rep.men}</td>
+				<td>{rep.average}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+{/await}
+</main>
--- front/App.svelte
+++ front/App.svelte
@@ -1,27 +1,50 @@
 <script>
-import interfazJHG from './apiJHG.svelte';
-export let name;
+	import interfazJHG from "./apiJHG.svelte";
+	import ApiRMA from "./ApiRMA.svelte";
+
+	export let name;
+
 </script>
 
 <main>
-	<h1>SOS 2122-32</h1>
+	<h1>{name}</h1>
 	<h2>Equipo</h2>
-	<p><a href="https://github.com/javighidalgo01">Javier Hidalgo García</a></p> 
-	<p>Rafael Molino Alvarez</p>
+	<p><a href="https://github.com/javighidalgo01">Javier Hidalgo García</a></p>
+	<p><a href="https://github.com/rafamolino">Rafael Molino Alvarez</a></p>
 	<p>Sergio Jesús García Blanco</p>
 	<h2>Descripción del proyecto</h2>
-	<p>Nuestras fuentes de información están orientadas a analizar la relación que existe entre el tiempo dedicado al trabajo doméstico, repetidores de primaria y la tasa de finalización de primaria globales.</p>
+	<p>
+		Nuestras fuentes de información están orientadas a analizar la relación
+		que existe entre el tiempo dedicado al trabajo doméstico, repetidores de
+		primaria y la tasa de finalización de primaria globales.
+	</p>
 	<h2>Repositorio</h2>
-	<p><a href="https://github.com/gti-sos/SOS2122-32">https://github.com/gti-sos/SOS2122-32 </a></p>
+	<p>
+		<a href="https://github.com/gti-sos/SOS2122-32"
+			>https://github.com/gti-sos/SOS2122-32
+		</a>
+	</p>
 	<h2>URL</h2>
-	<p><a href="http://sos2122-32.herokuapp.com/"> http://sos2122-32.herokuapp.com/</a></p>
+	<p>
+		<a href="http://sos2122-32.herokuapp.com/">
+			http://sos2122-32.herokuapp.com/</a
+		>
+	</p>
 	<h2>APIs</h2>
 	<p>Javier Hidalgo García</p>
-	<a href="http://sos2122-32.herokuapp.com/api/v1/housework-stats/docs"><button color="blue">API v1</button></a>
+	<a href="http://sos2122-32.herokuapp.com/api/v1/housework-stats/docs"
+		><button color="blue">API v1</button></a
+	>
 	<a href="/javhidgar/apiJHG.svelte"><button>Interfaz</button></a>
-	<interfazJHG />
-</main>
+	<p>Rafael Molino Alvarez</p>
+	<a href="http://sos2122-32.herokuapp.com/api/v1/repeaters-stats/docs"
+		><button color="blue">API v1</button></a
+	>
+	<a href="https://sos2122-32.herokuapp.com/#/repeaters-repeaters"><button>Interfaz</button></a>
+	
+	
 
+</main>
 
 <style>
 	main {
