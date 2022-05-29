a7220ae916ddccb6d645a8fa244a6c541e072b34
Sergarbla <sergar3d@gmail.com>
2022-05-29 21:39:45 +0200

    Editado

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -3,16 +3,17 @@
 	import apiJHG from "./apiJHG.svelte";
 	import Home from "./Home.svelte";
 	import ApiRMA from "./rafmolalv/ApiRMA.svelte";
-	import EditApiSGB from "./editApiSGB.svelte";
+	import EditApiSGB from "./sergarbla/editApiSGB.svelte";
 	import EditApiJHG from "./EditApiJHG.svelte";
 	import EditApiRMA from "./rafmolalv/EditApiRMA.svelte";
 	import Info from "./Info.svelte";
-	import ApiSgb from "./apiSGB.svelte";
+	import ApiSgb from "./sergarbla/apiSGB.svelte";
 	import Mygraph from "./Mygraph.svelte";
 	import MygraphJHG from "./MygraphJHG.svelte";
 	import MygraphRma from "./rafmolalv/MygraphRMA.svelte";
 	import MygraphRma2 from "./rafmolalv/MygraphRMA2.svelte";
-	import MygraphSGB from "./MygraphSGB.svelte";
+	import MygraphSGB from "./sergarbla/MygraphSGB.svelte";
+	import MygraphSGB2 from "./sergarbla/MygraphSGB2.svelte";
 	import MygraphJHGv2 from "./MygraphJHGv2.svelte";
 	import Integracion1RMA from "./rafmolalv/PublicExpenditureGraph.svelte";
 	import Integracion2RMA from "./rafmolalv/EnergyConsumptionGraph.svelte";
@@ -20,6 +21,8 @@
 	import ApiExterna2 from "./rafmolalv/ApiExterna2.svelte";
 	import ApiExterna3 from "./rafmolalv/ApiExterna3.svelte";
 	import ApiExterna4 from "./rafmolalv/ApiExterna4.svelte";
+	import ExternalApi1 from "./sergarbla/ExternalApi1.svelte";
+	import ExternalApi2 from "./sergarbla/ExternalApi2.svelte";
 	import GraphCo2Stats from "./GraphCo2Stats.svelte";
 	import GraphAirPollutionStats from "./GraphAirPollutionStats.svelte";
 	import GraphExternalAPI1 from "./GraphExternalAPI1.svelte";
@@ -44,6 +47,7 @@
 		"/graphRMA": MygraphRma,
 		"/graphRMA2": MygraphRma2,
 		"/graphSGB": MygraphSGB,
+		"/graphSGB2": MygraphSGB2,
 		"/graphJHGv2": MygraphJHGv2,
 		"/Integracion1RMA": Integracion1RMA,
 		"/Integracion2RMA": Integracion2RMA,
@@ -51,6 +55,8 @@
 		"/ApiExterna2": ApiExterna2,
 		"/ApiExterna3": ApiExterna3,
 		"/ApiExterna4": ApiExterna4,
+		"/ExternalApi1":ExternalApi1,
+		"/ExternalApi2":ExternalApi2,
 		"/graphCo2Stats": GraphCo2Stats,
 		"/graphAirPollutionStats": GraphAirPollutionStats,
 		"/graphExternalAPI": GraphExternalAPI1,
