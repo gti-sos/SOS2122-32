b250ddc0e8df21605e085668ef43e07a7e0f71dc
Sergarbla <sergar3d@gmail.com>
2022-05-30 11:24:33 +0200

    Integracion del video en About

--- src/front/About.svelte
+++ src/front/About.svelte
@@ -3,3 +3,7 @@
 <br>
 <br>
 <a href="https://www.youtube.com/watch?v=J4ajHs9wiOo"> Video Rafael Molino Alvarez</a>
+<br>
+<br>
+<br>
+<a href="https://youtu.be/3TGDN4U34wA"> Video Sergio Jesús García Blanco</a>
